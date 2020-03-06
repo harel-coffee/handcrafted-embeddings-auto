08b91b99fec8b3c05beaa51bda9b14480910c403
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2020-03-05 20:41:23 -0600

    normalized handcrafted embeddings

---
+++ handcrafted/normalized.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn import preprocessing
+import os, pathlib, shutil
+
+mDataTypes = ["java-large"]
+mDataCats = ["training"]
+mTargets = ["toString","equals","setUp"]
+
+for jt in mDataTypes:
+    for jc in mDataCats:
+        for mtarget in mTargets:
+            mPath = "../data/handcrafted/" + jt + "/" + jc
+            fname = mPath + "/count/" + mtarget + ".csv"
+            df = pd.read_csv(fname, header=None)
+
+            #normalized dataframe
+            df_norm = df.copy()
+            for c in df_norm.columns[2:-1]:
+                df_norm[[c]] = preprocessing.StandardScaler().fit_transform(df_norm[[c]])
+            fname = mPath + "/norm/" + mtarget + ".csv"
+            pathlib.Path(os.path.dirname(fname)).mkdir(parents=True, exist_ok=True)
+            df_norm.to_csv(fname, index=False, header=False)
+
+            #binary dataframe
+            df_binary = df.copy()
+            for c in df_binary.columns[2:-1]:
+                df_binary[c] = df_binary[c].apply(lambda x: 1 if x>0 else 0)
+            fname = mPath + "/binary/" + mtarget + ".csv"
+            pathlib.Path(os.path.dirname(fname)).mkdir(parents=True, exist_ok=True)
+            df_binary.to_csv(fname, index=False, header=False)
+
+            print("Done: ", mtarget, len(df_norm), len(df_binary))
+

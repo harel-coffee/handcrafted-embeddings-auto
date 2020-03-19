31beb46277b0b173ab705ef6bdf90ac6213bebed
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2020-03-19 01:16:01 -0500

    select random 1k jl top-20 methods

--- ncopy.py
+++ ncopy.py
@@ -1,26 +1,21 @@
 import pandas as pd
-import os, pathlib, shutil
+import os, pathlib, shutil, glob
 
 mDataTypes = ["java-large"]
-mDataCats = ["training"]
-
-mTargets = ["nonTarget", "equals", "setUp", "toString"]
 mN = 1000
 
-def saveOnCopy(pathList, mtarget):
-    for p1 in pathList:
-        if not os.path.exists(p1):
-            p1 = p1.replace("_space_", " ")
-            p1 = p1.replace("_comma_", ",")
+def saveOnCopy(mpaths, mtarget):
+    for p1 in mpaths:
         p2 = p1.replace("/transforms/Methods/", "/handcrafted/t"+str(mN)+"/"+mtarget+"/")
         pathlib.Path(os.path.dirname(p2)).mkdir(parents=True, exist_ok=True)
         shutil.copy2(p1,p2)
 
 for jt in mDataTypes:
-    for jc in mDataCats:
-        for mtarget in mTargets:
-            fname = jt + "/" + jc + "/" + mtarget
-            print (fname)
-            mpath = "data/nrandom/" + fname + ".txt"
-            mdf = pd.read_csv(mpath, sep=",", names=["path"])
-            saveOnCopy(list(mdf.path), mtarget)
+    fname = "data/nrandom/" + jt + "/*.txt"
+    mTargetFiles = glob.glob(fname)
+    for fname in mTargetFiles:
+        mtarget = (fname.split("/")[-1]).replace(".txt","")
+        mdf = pd.read_csv(fname, sep=",", names=["path"])
+        saveOnCopy(list(mdf.path), mtarget)
+        print("Done:", mtarget)
+
--- nrandom.py
+++ nrandom.py
@@ -1,33 +1,45 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import os, pathlib, shutil
+import os, pathlib, shutil, random
 from sklearn.utils import shuffle
-mRS = int(datetime.now().timestamp()) #42
+mRS = 42 #int(datetime.now().timestamp())
+random.seed(mRS)
 np.random.seed(mRS)
 
 mDataTypes = ["java-large"]
-mDataCats = ["training"]
+mDataCats = ["training", "validation", "test"]
 
-mTargets = ["equals", "setUp", "toString"]
 mN = 1000
+mTargets = ["equals", "main", "setUp", "onCreate", "toString", "run", "hashCode", "init", "execute", "get", "close", "start", "add", "write", "create", "tearDown", "clear", "read", "reset", "update"]
 
-def saveRadomSample(tdf, fname):
-    print(fname)
-    sdf = shuffle(tdf, n_samples=mN, random_state=mRS)
-    sdf = sdf.drop(['method', 'size'], axis=1)
+def saveNRadomSample(mpaths, fname):
+    np.random.shuffle(mpaths)
+    npaths = random.sample(population=mpaths, k=mN)
     fname = "data/nrandom/" + fname + ".txt"
     pathlib.Path(os.path.dirname(fname)).mkdir(parents=True, exist_ok=True)
-    sdf.to_csv(fname, header=None, index=None)
+    with open(fname, 'w') as f:
+        for tpath in npaths:
+            f.write("%s\n" % tpath)
+    print("Done: ", fname, len(set(npaths)))
+
+def getDataframe():
+    dfs = []
+    for jt in mDataTypes:
+        for jc in mDataCats:
+            fpath = "data/filter/" + jt + "-" + jc + ".csv"
+            fdf = pd.read_csv(fpath, sep=",", names=["path","method","size"])
+            fdf = fdf[~fdf['path'].astype(str).str.contains("space|comma")]
+            dfs.append(fdf)
+    mdf = pd.concat(dfs, axis=0, ignore_index=True)
+    return mdf
 
 for jt in mDataTypes:
-    for jc in mDataCats:
-        mpath = "data/filter/" + jt + "-" + jc + ".csv"
-        mdf = pd.read_csv(mpath, sep=",", names=["path","method","size"])
-        for mtarget in mTargets:
-            tdf = mdf.loc[mdf['method'] == mtarget]
-            fname = jt + "/" + jc + "/" + mtarget
-            saveRadomSample(tdf, fname)
-        ntdf = mdf.loc[~mdf['method'].isin(mTargets)]
-        saveRadomSample(ntdf, jt + "/" + jc + "/nonTarget")
+    mdf = getDataframe()
+    for mtarget in mTargets:
+        tdf = mdf.loc[mdf['method'] == mtarget]
+        fname = jt + "/" + mtarget
+        saveNRadomSample(list(tdf.path), fname)
+    ntdf = mdf.loc[~mdf['method'].isin(mTargets)]
+    saveNRadomSample(list(ntdf.path), jt + "/nonTarget")
 
